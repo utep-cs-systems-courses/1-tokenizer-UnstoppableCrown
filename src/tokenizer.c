6ef8465a6d946bd37659ab220d1938c93bf70990
Jacob <jagonzalez63@miners.utep.edu>
2023-02-15 13:01:53 -0700

    First

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,21 +6,33 @@
 //returns 1 if c is a white space
 int space_char(char c)
 {
-  if(c == " " || c == "\t")
+  if(c == ' ' || c == '\t')
     {
       return 1;
     }
   return 0;
 }
-
+//returns 1 if c is a non white space, non zero character
 int non_space_char(char c)
 {
-  
+  if(space_char(c) == 1 || c == '0')
+    {
+      return 0;
+    }
+  return 1;
 }
-
+//returns a pointer to the first character of the next word 
 char *word_start(char *str)
 {
-  
+  for(int i = 0; *(str +1) != '\0'; i++)
+    {
+      if(non_space_char(*str))
+	{
+	  return &*str;
+	}
+      *str = *(str + i);
+    }
+      return 0;
 }
 
 char *word_terminator(char *word)
