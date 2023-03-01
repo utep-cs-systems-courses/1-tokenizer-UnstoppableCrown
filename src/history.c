eae5daf478b3250b1066aca3c59cb9369252ed2c
Jacob <jagonzalez63@miners.utep.edu>
2023-02-28 20:48:01 -0700

    history_overwrites_all_previous_with_new_error_most_likely_in_add_history

--- src/history.c
+++ src/history.c
@@ -5,22 +5,45 @@
 
 List *init_history()
 {
-
+  List *list = malloc(sizeof(List));
+  list->root = malloc(sizeof(Item));
+  return list;
 }
 
 void add_history(List *list, char *str)
 {
-
+  int id = 1;
+  Item *temp = list->root; 
+  while (temp->next) {
+    temp = temp->next;
+    id++;
+  }
+  //printf("%d", id);
+  temp->next = malloc(sizeof(Item));
+  temp->next->str = str;
+  temp->next->id = id;
+  //printf("\n%s", temp->next->str);
+  
 }
 
 char *get_history(List *list, int id)
 {
-
+  Item *temp = list->root;
+  while (temp) {
+    if(temp->id == id) {
+      return temp->str;
+    }
+    temp = temp->next;
+  }
 }
 
 void print_history(List *list)
 {
-
+  Item *temp = list->root->next;
+  while(temp) {
+    printf("%d. %s", temp->id, temp->str);
+    temp = temp->next;
+  }
 }
 
 void free_history(List *list)
--- src/uimain.c
+++ src/uimain.c
@@ -5,22 +5,55 @@
 
 int main()
 {
+  printf("Hello, ");
+  
+  List *history = init_history();
   char line[100];
-  printf("Hello\n> ");
-  fgets(line, sizeof(line), stdin);
+  char sentence[100];
+  while(1) {
+    printf("please enter the action you would like to take:\nTokenize(T)\nCheck History(H)\nAccess a specific history(!id) id is the id of the specific item\nQuit(Q)\n> ");
+    fgets(line, sizeof(line), stdin);
 
-  //printf("tests for word_terminator\n");
-  //printf( &(*word_terminator(line)));
-  //printf("after word_terminator");
-  //count_words(line);
-  //printf("there are: ");
-  //int c = count_words(line);
-  //printf("there are %d",c);
-  //printf(" words\n");
-  char *out = copy_str(line, 5);
-  printf(out);
-
-  puts(line);
+    //printf("tests for word_terminator\n");
+    //printf( &(*word_terminator(line)));
+    //printf("after word_terminator");
+    //count_words(line);
+    //printf("there are: ");
+    //int c = count_words(line);
+    //printf("there are %d",c);
+    //printf(" words\n");
+    //char *out = copy_str(line, 5);
+    //printf(out);
+    //char** tokens = tokenize(line);
+    //print_tokens(tokens);
+    //puts(line);
+    if(line[0] == 'T' || line[0] == 't') {
+      printf("Please enter your sentence\n> ");
+      fgets(sentence, sizeof(sentence), stdin);
+      char **tokens = tokenize(sentence);
+      print_tokens(tokens);
+      add_history(history, sentence);
+      printf("\n%sadded to history\n", sentence);
+      free_tokens(tokens);
+    }
+    if(line[0] == 'H' || line[0] == 'h') {
+      print_history(history);
+    }
+    if(line[0] == '!') {
+      int id = atoi(line+1);
+      char *hist = get_history(history, id);
+      char **tokens = tokenize(hist);
+      printf("You picked ID: %d\n", id);
+      printf("History retrieved: %s\n", hist);
+      printf("History tokenized: \n");
+      print_tokens(tokens);
+      free_tokens(tokens);
+    }
+    if(line[0] == 'Q' || line[0] == 'q') {
+      goto done;
+    }
+    printf("\nWhat would you like to do next, ");
+  }
  done:
   return 0;
 }
